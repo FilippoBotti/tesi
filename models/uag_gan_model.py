3070931219051fecf24e7499bcdf8b3f8fd768c9
FilippoBotti <filippo.botti.itis@gmail.com>
2022-10-17 16:45:31 +0200

    Deleted norm layer in netG_img

--- models/uag_gan_model.py
+++ models/uag_gan_model.py
@@ -330,15 +330,14 @@ class UAGGANModel(BaseModel):
 
         self.netG_att_A = uag.define_net_att(opt.input_nc,
                                              opt.ngf,
-                                             norm="none",
+                                             norm=opt.norm,
                                              init_type=opt.init_type,
                                              init_gain=opt.init_gain,
                                              gpu_ids=opt.gpu_ids)
-       
 
         self.netG_att_B = uag.define_net_att(opt.input_nc,
                                              opt.ngf,
-                                             norm="none",
+                                             norm=opt.norm,
                                              init_type=opt.init_type,
                                              init_gain=opt.init_gain,
                                              gpu_ids=opt.gpu_ids)
@@ -346,7 +345,7 @@ class UAGGANModel(BaseModel):
         self.netG_img_A = uag.define_net_img(opt.input_nc,
                                              opt.output_nc,
                                              opt.ngf,
-                                             norm=opt.norm,
+                                             norm='none',
                                              init_type=opt.init_type,
                                              init_gain=opt.init_gain,
                                              gpu_ids=opt.gpu_ids)
@@ -354,7 +353,7 @@ class UAGGANModel(BaseModel):
         self.netG_img_B = uag.define_net_img(opt.input_nc,
                                              opt.output_nc,
                                              opt.ngf,
-                                             norm=opt.norm,
+                                             norm='none',
                                              init_type=opt.init_type,
                                              init_gain=opt.init_gain,
                                              gpu_ids=opt.gpu_ids)
